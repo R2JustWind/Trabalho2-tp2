c63a6e36b5ad5bd17bef20ff79fbfd9bbf1629c8
arthur <arthursmfreire@gmail.com>
2025-10-16 15:32:24 -0300

    Corrigido erro do teste 1

--- backup.cpp
+++ backup.cpp
@@ -4,9 +4,16 @@
  * \file testa_backup.cpp
  */
 
-
+#include <cstdio>
+#include <cstring>
+#include <cassert>
 #include "backup.hpp"
 
 int FazBackup(const char* source, const char* destination){
+    FILE* file = fopen(source, "r");
+    
+    if (file == nullptr) {
+        return bImpossible;
+    }
     return bDoNothing;
 }
