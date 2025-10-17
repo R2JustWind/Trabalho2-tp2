2dc5c691c587727d32995f2eda2b314e0179ca84
Arthur Soares Magalhães Freire <arthursmfreire@gmail.com>
2025-10-16 21:10:18 -0300

    Caso de teste para a quinta coluna da tabela

--- testa_backup.cpp
+++ testa_backup.cpp
@@ -92,3 +92,23 @@ TEST_CASE("Ambos possuem o arquivo de mesmo tamanho", "[FazBackup]") {
 
     CleanUp();
 }
+
+TEST_CASE("Ambos possuem o arquivo mas o de PenDrive é maior", "[FazBackup]") {
+    CleanUp();
+
+    fs::create_directories("HD");
+    fs::create_directories("PenDrive");
+    
+    std::ofstream("HD/arquivo1.txt") << "dados menores";
+    std::string source_hd = fs::absolute("HD/arquivo1.txt").string();
+
+    std::ofstream("PenDrive/arquivo1.txt") << "dados um pouco maiores";
+
+    std::ofstream param_file("backup.parm");
+    param_file << "FAZ_BACKUP=TRUE\n" + source_hd + "\n";
+    param_file.close();
+
+    REQUIRE(FazBackup("PenDrive/") == bError);
+
+    CleanUp();
+}
