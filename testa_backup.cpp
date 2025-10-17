d871f433b8808cfead99fe89e6abaeda877135d2
Arthur Soares Magalhães Freire <arthursmfreire@gmail.com>
2025-10-17 16:05:12 -0300

    Caso de teste para a última coluna da tabela

--- testa_backup.cpp
+++ testa_backup.cpp
@@ -246,3 +246,22 @@ TEST_CASE("Restauração: Nenhum dos 2 possui o arquivo", "[FazBackup]") {
 
     REQUIRE(FazBackup("PenDrive/") == bError);
 }
+
+TEST_CASE("Restauração: Apenas o pendrive possui o arquivo", "[FazBackup]") {
+    CleanUp();
+
+    fs::create_directories("HD");
+    fs::create_directories("PenDrive");
+
+    std::ofstream("PenDrive/arquivo1.txt") << "dados no pendrive";
+
+    std::string source_hd = fs::absolute("HD/arquivo1.txt").string();
+
+    std::ofstream param_file("backup.parm");
+    param_file << "FAZ_BACKUP=FALSE\n" + source_hd + "\n";
+    param_file.close();
+
+    REQUIRE(FazBackup("PenDrive/") == bBackupToHD);
+
+    CleanUp();
+}
